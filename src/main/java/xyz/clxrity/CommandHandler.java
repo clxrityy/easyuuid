7f34a67884422797fcff029149759d9978b002b1
clxrityy <clxrityxyz@gmail.com>
2023-03-01 13:53:31 -0500

    tes

--- src/main/java/xyz/clxrity/CommandHandler.java
+++ src/main/java/xyz/clxrity/CommandHandler.java
@@ -1,8 +1,12 @@
 package xyz.clxrity;
+
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import net.md_5.bungee.api.ChatColor;
 
 public class CommandHandler implements CommandExecutor {
     @Override
@@ -13,7 +17,9 @@ public class CommandHandler implements CommandExecutor {
             Player player = (Player) sender;
             String target = args[0];
             Plugin.LOGGER.info(player + " is searching for the UUID of " + target);
-            player.sendMessage("Searching for the UUID of " + target);
+            player.sendMessage("Searching for the UUID of " + ChatColor.AQUA + target);
+            String uuid = Bukkit.getPlayer(target).getUniqueId().toString();
+            player.sendMessage(ChatColor.AQUA + target + "'s " + " UUID:" + "\n" + ChatColor.GREEN + uuid);
 
             return true;
         }
