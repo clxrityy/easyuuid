a0e1c0d2b44d41e055241c49876ce2b4cfdc8033
clxrityy <clxrityxyz@gmail.com>
2023-03-02 02:27:30 -0500

    config operations

--- src/main/java/xyz/clxrity/Plugin.java
+++ src/main/java/xyz/clxrity/Plugin.java
@@ -3,24 +3,28 @@ package xyz.clxrity;
 import java.util.logging.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import xyz.clxrity.config.Config;
+
 /*
  * easyuuid java plugin
  */
-public class Plugin extends JavaPlugin
-{
+public class Plugin extends JavaPlugin {
   public static final Logger LOGGER = Logger.getLogger("easyuuid");
-  
+
   private CommandHandler commandHandler;
 
-  public void onEnable()
-  {
+  public void onEnable() {
     LOGGER.info("easyuuid enabled");
+    Config.setup();
+    Config.get().addDefault("fetch", "op");
+    Config.get().addDefault("uuid", true);
+    Config.get().options().copyDefaults();
+    Config.save();
     commandHandler = new CommandHandler();
     getCommand("fetch").setExecutor(commandHandler);
   }
 
-  public void onDisable()
-  {
+  public void onDisable() {
     LOGGER.info("easyuuid disabled");
   }
 }
